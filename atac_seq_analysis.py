34fa1145680538a9fd1287ed7da7cbef3cb60d87
james <jadler2@uoregon.edu>
2022-03-01 14:04:02 -0800

    refactor thresholds and transofrmations

--- atac_seq_analysis.py
+++ atac_seq_analysis.py
@@ -25,13 +25,14 @@ def parse_args():
 
     parser.add_argument('--metadata',help='Metadata file')
     parser.add_argument('--atac_counts',help='ATAC-seq counts data')
-    parser.add_argument('--filter_threshold',help='Minimum counts per sample.',type=int,default=0)
-    parser.add_argument('--n_components',help='Number PCA components.',type=int,default=50)
+    parser.add_argument('--lower_filter_threshold',help='Minimum counts per sample.',type=int,default=0)
+    parser.add_argument('--upper_filter_threshold',help='Maximum counts per sample.',type=int,default=None)
+    parser.add_argument('--n_components',help='Number PCA components.',type=int,default=20)
 
     parser.add_argument('--umap_first_pc',help='First principal component to use in UMAP.',type=int,default=0)
-    parser.add_argument('--umap_last_pc',help='Last principal component to use in UMAP.',type=int,default=12)
+    parser.add_argument('--umap_last_pc',help='Last principal component to use in UMAP.',type=int,default=14)
     parser.add_argument('--svc_first_pc',help='First principal component to use in SVC.',type=int,default=0)
-    parser.add_argument('--svc_last_pc',help='Last principal component to use in SVC.',type=int,default=12)
+    parser.add_argument('--svc_last_pc',help='Last principal component to use in SVC.',type=int,default=14)
 
     parser.add_argument('--umap_color',help='Metadata column to color UMAP by.',type=str,default='sample_id')
     parser.add_argument('--dim_reduce_image',help='Name of dimension reduced image.',type=str,default='dim-reduce.png')
@@ -60,11 +61,16 @@ def process_metadata(df,metadata):
     return metadata
 
 # filter
-def filter_matrix(df,metadata,filter):
+def filter_matrix(df,metadata,lower_filter,upper_high):
     """
-    Removes peaks with fewer than 'filter' reads.
+    Removes samples with more than lower_filter and fewer than upper_filter total reads. Removes biases
+    introduced due to varying levels of total reads.
     """
-    count_mask = df.sum(axis=0) > filter
+    # TODO: could make this an automatic calculation (ie include the middle 95%)
+    if upper_high is not None:
+        count_mask = (df.sum(axis=0) > lower_filter) & (df.sum(axis=0) < upper_high)
+    else:
+        count_mask = (df.sum(axis=0) > lower_filter)
 
     filtered_df = df.loc[:,count_mask]
     filtered_metadata = metadata.loc[count_mask,:]
@@ -73,12 +79,12 @@ def filter_matrix(df,metadata,filter):
     return filtered_df, filtered_metadata
 
 # normalize
-def normalize_matrix(df):
+def normalize_sample_counts(df):
     """
-    normalize matrix so each sample has equal number of reads
+    Normalize sample counts to 1,000,000.
     """
     # normalize
-    normalized_df = df * df.sum(axis=0).min() / df.sum(axis=0).values
+    normalized_df = df * 1000000 / df.sum(axis=0).values
     
     return normalized_df
 
@@ -96,9 +102,9 @@ def df_transformation(df):
     perform boxcox transformation
     """
     trans_df = PowerTransformer(method="box-cox").fit_transform(df+1)
-    
+
     trans_df = trans_df.transpose()
-    trans_df = trans_df / trans_df.max(axis=0)
+    trans_df = trans_df / np.absolute(trans_df).max(axis=0)
     trans_df = trans_df - trans_df.mean(axis=0)
     trans_df = trans_df.transpose()
 
@@ -111,6 +117,7 @@ def reduce_dimensions(df,n_components):
     """
     pca = PCA(n_components=n_components)
     components = pca.fit_transform(df)
+
     return components
 
 # UMAP
@@ -182,8 +189,8 @@ if __name__ == '__main__':
     metdaata = process_metadata(df,metadata)
 
     # normalize and filter data
-    df, metadata = filter_matrix(df,metadata,args.filter_threshold)
-    df = normalize_matrix(df)
+    df, metadata = filter_matrix(df,metadata,args.lower_filter_threshold,args.upper_filter_threshold)
+    df = normalize_sample_counts(df)
     
     # transform data
     df = df_transformation(df)
